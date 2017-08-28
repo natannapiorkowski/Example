4fcde97b5a8636889c4b352e36fb02b0a07b8b9b
ASPP Student <student@localhost>
2017-08-28 18:18:28 +0300

    setup added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='package_1',
+      version='0.1',
+      description='blah blah blah example',
+      url='https://github.com/natannapiorkowski/Example.git',
+      author='xxxxx',
+      author_email='xxxx@example.com',
+      license='MIT',
+      packages=['package_1'],
+      zip_safe=False)
